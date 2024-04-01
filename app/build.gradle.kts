24af9779c20e089c8c0770438c175a684a25f0ea
Sth-bear <pencil2468@gmail.com>
2024-04-01 18:26:38 +0900

    마이페이지 xml작업

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,14 +33,9 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
-
-    buildFeatures {
-        viewBinding = true
-    }
 }
 
 dependencies {
-
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
@@ -49,5 +44,4 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation ("de.hdodenhof:circleimageview:3.1.0")
 }
