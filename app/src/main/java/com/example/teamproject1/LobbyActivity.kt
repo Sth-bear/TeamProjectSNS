c5f7415147869a7d696ea494f5de93380a43a03f
Hyuna <kmtso434@naver.com>
2024-04-02 15:43:55 +0900

    환경설정 팝업창이 추가되었습니다. res/layout 아래에 menu파일이 추가되었습니다. menu.xml 아이템 파일이 새로 추가되었고, LobbyActivity MyPageActivity에 버튼 기능 및 환경설정팝업창 추가되었습니다. lobby.xml은 ivsetup 아이디가 btnsetup으로 변경되었습니다.

--- app/src/main/java/com/example/teamproject1/LobbyActivity.kt
+++ app/src/main/java/com/example/teamproject1/LobbyActivity.kt
@@ -1,7 +1,10 @@
 package com.example.teamproject1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.PopupMenu
+import android.widget.Toast
 import com.example.teamproject1.databinding.ActivityLobbyBinding
 
 //메인화면
@@ -12,5 +15,34 @@ class LobbyActivity : AppCompatActivity() {
         binding = ActivityLobbyBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        //환경설정 팝업
+        binding.btnSetUp.setOnClickListener {
+            val popupMenu = PopupMenu(this, it)
+            popupMenu.menuInflater.inflate(R.menu.menu, popupMenu.menu)
+            popupMenu.setOnMenuItemClickListener { menuItem ->
+
+                when (menuItem.itemId) {
+                    R.id.action_menu1 -> {
+                        //여기에 언어변경 코딩 입력해주세요
+                        Toast.makeText(this, "언어 변경이 완료되었습니다", Toast.LENGTH_SHORT).show()
+                        true
+                    }
+
+                    else -> {
+                        //여기에 야간모드 코딩 입력해주세요
+                        Toast.makeText(this, "야간모드로 변경되었습니다", Toast.LENGTH_SHORT).show()
+                        true
+                    }
+                }
+            }
+            popupMenu.show()
+        }
+
+        //마이페이지 버튼
+        binding.ivmyPage.setOnClickListener{
+            val intent = Intent(this, MyPageActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/teamproject1/MyPageActivity.kt
+++ app/src/main/java/com/example/teamproject1/MyPageActivity.kt
@@ -1,7 +1,10 @@
 package com.example.teamproject1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.PopupMenu
+import android.widget.Toast
 import com.example.teamproject1.databinding.ActivityMyPageBinding
 
 //개인정보
@@ -12,5 +15,34 @@ class MyPageActivity : AppCompatActivity() {
         binding = ActivityMyPageBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        //환경설정 팝업
+        binding.btnSetUp.setOnClickListener {
+            val popupMenu = PopupMenu(this, it)
+            popupMenu.menuInflater.inflate(R.menu.menu, popupMenu.menu)
+            popupMenu.setOnMenuItemClickListener { menuItem ->
+
+                when (menuItem.itemId) {
+                    R.id.action_menu1 -> {
+                        //여기에 언어변경 코딩 입력해주세요
+                        Toast.makeText(this, "언어 변경이 완료되었습니다", Toast.LENGTH_SHORT).show()
+                        true
+                    }
+
+                    else -> {
+                        //여기에 야간모드 코딩 입력해주세요
+                        Toast.makeText(this, "야간모드로 변경되었습니다", Toast.LENGTH_SHORT).show()
+                        true
+                    }
+                }
+            }
+            popupMenu.show()
+        }
+
+        //뒤로가기 버튼
+        binding.btnBack.setOnClickListener{
+            val intent = Intent(this, LobbyActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
