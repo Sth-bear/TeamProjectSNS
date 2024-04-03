d3a3de40597bd16d11e26f9e676b4a603170b3b9
DF_Kim <84631435+Combro-Kim@users.noreply.github.com>
2024-04-03 11:10:23 +0900

    animation 버전 예외처리 (#17)
    * lobbyActivity에 Horizontal Scroll 추가
    
    * PostActivity layout design
    
    * post activity layout design

--- app/src/main/java/com/example/teamproject1/DetailActivity.kt
+++ app/src/main/java/com/example/teamproject1/DetailActivity.kt
@@ -1,6 +1,7 @@
 package com.example.teamproject1
 
 import android.app.Activity
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.teamproject1.databinding.ActivityDetailBinding
@@ -16,13 +17,16 @@ class DetailActivity : AppCompatActivity() {
         binding = ActivityDetailBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-        //animation open
-        overrideActivityTransition(Activity.OVERRIDE_TRANSITION_OPEN, R.anim.slide_right_enter, R.anim.slide_none)
+        if (Build.VERSION.SDK_INT >= 34) {
+            overrideActivityTransition(Activity.OVERRIDE_TRANSITION_OPEN, R.anim.slide_right_enter, R.anim.slide_none)
+        } else {
+            overridePendingTransition(R.anim.slide_right_enter, R.anim.slide_none)
+        }
 
         val userId = intent.getStringExtra("userId")
         val userName = userId?.let { checkName(it) }
-        val userImage =  userList.find{it .id == userId}?.userImage
-        val userIntro = userList.find{it.id == userId}?.userIntro
+        val userImage = userList.find { it.id == userId }?.userImage
+        val userIntro = userList.find { it.id == userId }?.userIntro
 
         if (userImage != null) {
             binding.ivUserImage.setImageResource(userImage)
@@ -34,15 +38,11 @@ class DetailActivity : AppCompatActivity() {
         binding.tvId.text = userId
         binding.tvId2.text = userId
 
-        postList.find { it.id == userId}?.image?.let { binding.ivDetail.setImageResource(it) }
+        postList.find { it.id == userId }?.image?.let { binding.ivDetail.setImageResource(it) }
 
         binding.btnBack.setOnClickListener {
             finish()
         }
-
-
-
-
 
 
     }
@@ -50,6 +50,10 @@ class DetailActivity : AppCompatActivity() {
     override fun finish() {
         super.finish()
         //animation close
-        overrideActivityTransition(Activity.OVERRIDE_TRANSITION_CLOSE, R.anim.slide_none, R.anim.slide_right_exit)
+        if (Build.VERSION.SDK_INT >= 34) {
+            overrideActivityTransition(Activity.OVERRIDE_TRANSITION_CLOSE, R.anim.slide_none, R.anim.slide_right_exit)
+        } else {
+            overridePendingTransition(R.anim.slide_none, R.anim.slide_right_exit)
+        }
     }
 }
