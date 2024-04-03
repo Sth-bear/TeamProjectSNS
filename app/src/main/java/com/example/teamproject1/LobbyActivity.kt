5460e572abf296f451400ab30f09392b247520e7
Sth-bear <pencil2468@gmail.com>
2024-04-03 18:21:03 +0900

    Lobby 게시물 db연결

--- app/src/main/java/com/example/teamproject1/LobbyActivity.kt
+++ app/src/main/java/com/example/teamproject1/LobbyActivity.kt
@@ -7,8 +7,11 @@ import android.widget.PopupMenu
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatDelegate
+import com.example.teamproject1.PostList.postList
+import com.example.teamproject1.PostList.showInfo
 import com.example.teamproject1.UserList.userList
 import com.example.teamproject1.databinding.ActivityLobbyBinding
+import de.hdodenhof.circleimageview.CircleImageView
 
 //메인화면
 class LobbyActivity : AppCompatActivity() {
@@ -23,11 +26,39 @@ class LobbyActivity : AppCompatActivity() {
         binding.tvUserName.text = userList.find { it.id == loginId }?.username
         userList.find{it.id == loginId}?.userImage?.let { binding.ivToMyPage.setImageResource(it) }
 
+        //1번게시글
+        val (page1UserName,page1UserImage,page1PostImage) = showInfo("test2")
+        binding.tvName1.text = page1UserName
+        binding.ivUserImage1.setImageResource(page1UserImage)
+        binding.ivDetail1.setImageResource(page1PostImage)
         binding.ivDetail1.setOnClickListener {
             val intent = Intent(this, PostActivity::class.java)
-            intent.putExtra("userId","test1" )
+            intent.putExtra("userId","test2" )
             startActivity(intent)
         }
+
+        val(page2UserName,page2UserImage,page2PostImage) = showInfo("test3")
+        binding.tvName2.text = page2UserName
+        binding.ivUserImage2.setImageResource(page2UserImage)
+        binding.ivDetail2.setImageResource(page2PostImage)
+        binding.ivDetail2.setOnClickListener {
+            val intent = Intent(this, PostActivity::class.java)
+            intent.putExtra("userId", "test3")
+            startActivity(intent)
+        }
+
+        val(page3UserName,page3UserImage,page3PostImage) = showInfo("test4")
+        binding.tvName3.text = page3UserName
+        binding.ivUserImage3.setImageResource(page3UserImage)
+        binding.ivDetail3.setImageResource(page3PostImage)
+        binding.ivDetail3.setOnClickListener {
+            val intent = Intent(this, PostActivity::class.java)
+            intent.putExtra("userId", "test4")
+            startActivity(intent)
+        }
+
+
+
         binding.ivToMyPage.setOnClickListener {
             val intent = Intent(this, MyPageActivity::class.java)
             intent.putExtra("loginId", loginId)
--- app/src/main/java/com/example/teamproject1/PostInfo.kt
+++ app/src/main/java/com/example/teamproject1/PostInfo.kt
@@ -12,15 +12,22 @@ data class PostInfo(
 object PostList {
     private var nextIndex: Int = 1
     val postList = mutableListOf<PostInfo>(
-        PostInfo(postIndex = 1, id = "test1", image = R.drawable.test1_user_post_image,  postContent = "오늘은 날씨가 정말 좋구나 나만 혼자야 ㅠㅠㅠㅠㅠㅠ", name = "전지우", userImage = R.drawable.test1_user_image),
-        PostInfo(postIndex = 2, id = "test2", image = 123,  postContent = "오늘 날씨가 엄청 덥네요:: 대충 SNS문구", name = "한정혁", userImage = R.drawable.test1_user_image),
-        PostInfo(postIndex = 3, id = "test3", image = 12,  postContent = "물결 소리가 청량함과 평온을 선사하는 아침 해변, 내게는 무한한 자유로움을 상상케 해.", name = "설희아", userImage = R.drawable.test1_user_image),
-        PostInfo(postIndex = 4, id = "test4", image = 123,  postContent = "시간은 마치 바람 같아, 잡히지 않지만 느껴지는 순간, 그 순간이 소중한 거야.", name = "한우영", userImage = R.drawable.test1_user_image),
+        PostInfo(postIndex = 1, id = "test1", image = R.drawable.test3_user_post_image,  postContent = "오늘은 날씨가 정말 좋구나 나만 혼자야 ㅠㅠㅠㅠㅠㅠ", name = "전지우", userImage = R.drawable.test1_user_image),
+        PostInfo(postIndex = 2, id = "test2", image = R.drawable.iv_example,  postContent = "오늘 날씨가 엄청 덥네요! 패딩은 두고 오세요.오늘 날씨가 엄청 덥네요! 패딩은 두고 오세요.오늘 날씨가 엄청 덥네요! 패딩은 두고 오세요.", name = "한정혁", userImage = R.drawable.iv_example2),
+        PostInfo(postIndex = 3, id = "test3", image = R.drawable.test2_user_post_image,  postContent = "물결 소리가 청량함과 평온을 선사하는 아침 해변, 내게는 무한한 자유로움을 상상케 해.", name = "설희아", userImage = R.drawable.test2_user_image),
+        PostInfo(postIndex = 4, id = "test4", image = R.drawable.test3_user_post_image,  postContent = "쿠크크크크크", name = "한우영", userImage = R.drawable.test3_user_image),
     )
 
     fun addPost(id: String, image: Int, postContent: String, name: String) {
         val newPost = PostInfo(postIndex = nextIndex,id = id,image = image,postContent = postContent,name = name, userImage = 1)
         postList.add(newPost)
         nextIndex++
+    }
+
+    fun showInfo(id:String): Triple<String, Int, Int> {
+        val userName = postList.find { it.id == id }?.name ?: "Error"
+        val userImage = postList.find { it.id == id }?.userImage ?: -1
+        val postImage = postList.find { it.id == id }?.image ?: -1
+        return Triple(userName,userImage,postImage)
     }
 }
