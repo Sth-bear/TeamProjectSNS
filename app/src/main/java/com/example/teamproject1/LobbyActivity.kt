a418abefc8693064d98cb9b08e40897a3f0ecd3f
Hyuna <kmtso434@naver.com>
2024-04-03 13:25:39 +0900

    프로필 사진등록 기능입니다.

--- app/src/main/java/com/example/teamproject1/LobbyActivity.kt
+++ app/src/main/java/com/example/teamproject1/LobbyActivity.kt
@@ -1,6 +1,7 @@
 package com.example.teamproject1
 
 import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.PopupMenu
@@ -16,39 +17,11 @@ class LobbyActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-
-        //환경설정 팝업
-        binding.btnSetUp.setOnClickListener {
-            val popupMenu = PopupMenu(this, it)
-            popupMenu.menuInflater.inflate(R.menu.menu, popupMenu.menu)
-            popupMenu.setOnMenuItemClickListener { menuItem ->
-
-                when (menuItem.itemId) {
-                    R.id.action_menu1 -> {
-                        //여기에 언어변경 코딩 입력해주세요
-                        Toast.makeText(this, "언어 변경이 완료되었습니다", Toast.LENGTH_SHORT).show()
-                        true
-                    }
-
-                    else -> {
-                        //여기에 야간모드 코딩 입력해주세요
-                        Toast.makeText(this, "야간모드로 변경되었습니다", Toast.LENGTH_SHORT).show()
-                        true
-                    }
-                }
-            }
-            popupMenu.show()
-        }
-
-
-        binding.ivPostImage.setOnClickListener {
-            val intent = Intent(this, DetailActivity::class.java)
-            startActivity(intent)
-        }
-        binding.ivmyPage.setOnClickListener {
-
+        binding.ivToMyPage.setOnClickListener {
             val intent = Intent(this, MyPageActivity::class.java)
             startActivity(intent)
         }
+
+
     }
 }
--- app/src/main/java/com/example/teamproject1/MyPageActivity.kt
+++ app/src/main/java/com/example/teamproject1/MyPageActivity.kt
@@ -1,16 +1,59 @@
 package com.example.teamproject1
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.ImageView
 import com.example.teamproject1.databinding.ActivityMyPageBinding
+import java.io.IOException
 
 //개인정보
 class MyPageActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMyPageBinding
+
+    companion object {
+        private const val PICK_IMAGE_REQUEST = 1
+    }
+
+    //프로필 사진 업로드
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMyPageBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        binding.btnBack.setOnClickListener{
+            val intent = Intent(this, LobbyActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnProfile.setOnClickListener {
+            openGallery()
+        }
     }
+
+    private fun openGallery() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(intent, PICK_IMAGE_REQUEST)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK && data != null) {
+            val selectedImageUri: Uri? = data.data
+
+            try {
+                val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, selectedImageUri)
+                binding.ivUserImage.setImageBitmap(bitmap)
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
 }
