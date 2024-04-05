6b0b4ee68532609ef07db7f4a3ab63224cbd1f4e
LimChaem <159120268+LimChaem@users.noreply.github.com>
2024-04-05 20:55:09 +0900

    폰트 크기에 대한 레이아웃 대응 및 폰트 선택 기능 (#38)
    * text size 조정, 외부 font 적용
    
    * font 변경
    
    * Font Select

--- app/src/main/java/com/example/teamproject1/LobbyActivity.kt
+++ app/src/main/java/com/example/teamproject1/LobbyActivity.kt
@@ -16,11 +16,17 @@ import com.example.teamproject1.listview.ListAdapter
 class LobbyActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLobbyBinding
     private lateinit var adapter: ListAdapter
+    private lateinit var fontChange: FontChange
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLobbyBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+
+        fontChange = FontChange(this)
+        applyFontToAllViews(FontManager.getSelectedFont(this))
 
         val loginId = intent.getStringExtra("loginId")
         binding.tvUserName.text = userList.find { it.id == loginId }?.username
@@ -51,6 +57,10 @@ class LobbyActivity : AppCompatActivity() {
         }
     }
 
+    private fun applyFontToAllViews(selectedFont: String) {
+        fontChange.applyFontToTextView(selectedFont, findViewById(android.R.id.content))
+    }
+
     private fun initPost() {
         val postList = PostList.postList
         adapter = ListAdapter(this, postList) //initAdapter
@@ -75,7 +85,8 @@ class LobbyActivity : AppCompatActivity() {
                 )
             }
         }
-
+        // 로비 액티비티가 다시 시작될 때마다 폰트를 적용합니다.
+        applyFontToAllViews(FontManager.getSelectedFont(this))
     }
 
 }
